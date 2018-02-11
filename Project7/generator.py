c81492edd45885e840b45e78380d82cce1a3061e
PhilippMueller <muller-phil@gmx.net>
2018-02-11 14:47:17 +0100

    made dropout optional

--- Project7/generator.py
+++ Project7/generator.py
@@ -2,7 +2,8 @@ import tensorflow as tf
 
 from ops import *
 
-def generator(z, batch_size, z_dim=100, output_dim=[300,300], gf_dim=64, gfc_dim=1024, c_dim=3):
+def generator(z, batch_size, z_dim=100, output_dim=[300,300],
+                gf_dim=64, gfc_dim=1024, c_dim=3, dropout=False):
     """
     Args:
       sess: TensorFlow session
@@ -42,22 +43,26 @@ def generator(z, batch_size, z_dim=100, output_dim=[300,300], gf_dim=64, gfc_dim
 
         # use normaliziation and relu.
         h0 = tf.nn.relu(g_bn0(h0))
-        h0 = tf.layers.dropout(inputs=h0, rate=0.5)
+        if dropout:
+            h0 = tf.layers.dropout(inputs=h0, rate=0.5)
 
         h1, h1_w, h1_b = deconv2d(h0, [batch_size, s_h8, s_w8, gf_dim * 4],
                                     name='g_h1', with_w=True)
         h1 = tf.nn.relu(g_bn1(h1))
-        h1 = tf.layers.dropout(inputs=h1, rate=0.5)
+        if dropout:
+            h1 = tf.layers.dropout(inputs=h1, rate=0.5)
 
         h2, h2_w, h2_b = deconv2d(h1, [batch_size, s_h4, s_w4, gf_dim * 2],
                                     name='g_h2', with_w=True)
         h2 = tf.nn.relu(g_bn2(h2))
-        h2 = tf.layers.dropout(inputs=h2, rate=0.5)
+        if dropout:
+            h2 = tf.layers.dropout(inputs=h2, rate=0.5)
 
         h3, h3_w, h3_b = deconv2d(h2, [batch_size, s_h2, s_w2, gf_dim * 1],
                                     name='g_h3', with_w=True)
         h3 = tf.nn.relu(g_bn3(h3))
-        h3 = tf.layers.dropout(inputs=h3, rate=0.5)
+        if dropout:
+            h3 = tf.layers.dropout(inputs=h3, rate=0.5)
 
         h4, h4_w, h4_b = deconv2d(h3, [batch_size, s_h, s_w, c_dim],
                                     name='g_h4', with_w=True)
